9f8108f95e5620ea6f43f26f40c0f500cd24ebcf
Praneethshada <praneethreddyshada@gmail.com>
2026-06-05 03:04:50 +0530

    Add day 5

---
+++ SDE Sheet Challenge/Day 5/majority1.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int ans = 0;
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (count == 0) {
+                count = 1;
+                ans = nums[i];
+            } else if (ans == nums[i]) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return ans;
+    }
+}
